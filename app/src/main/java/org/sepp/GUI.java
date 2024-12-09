b02232ade2e0e4d4a4bb51d5814d26d1642fa52e
Nahi-git <nahiyandoeswork@gmail.com>
2024-12-09 21:05:33 +0000

    Made GUI class

---
+++ app/src/main/java/org/sepp/GUI.java
@@ -0,0 +1,22 @@
+package org.sepp;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main (String[] args){
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Prototype");
+        Scene scene = new Scene (layout, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+}
