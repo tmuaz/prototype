001f3fdf6db4e8ae6af5e237552362add044b656
Tahasin Muaz <tahasin-muaz@outlook.com>
2024-12-09 18:31:10 +0000

    Add javafx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 repositories {
@@ -32,6 +33,11 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+javafx {
+    version = "23.0.1"
+    modules = listOf("javafx.controls", "javafx.fxml")
 }
 
 application {
