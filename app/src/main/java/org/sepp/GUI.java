d8d9251a7699ec253892378a3bd59adbbcc01a34
Muaz <tahasin-muaz@outlook.com>
2024-12-10 15:57:47 +0000

    fix GUI commit

--- app/src/main/java/org/sepp/GUI.java
+++ app/src/main/java/org/sepp/GUI.java
@@ -1,8 +1,6 @@
 package org.sepp;
 
-import java.io.File;
 import java.util.Arrays;
-import java.util.List;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -13,203 +11,153 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import org.checkerframework.checker.units.qual.A;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class GUI extends Application {
-  public class Context {
-    Config config = null;
-    File runDirectory = null;
-    File selected = null;
+    public class Context{
+        Config config = null;
+        File runDirectory = null;
+        File selected = null;
 
-    public Context() {}
+        public Context(){
 
-    public List<String> getConfigFileNames() {
-      File[] files = Config.listConfigs();
-      return Arrays.stream(files)
-          .map(
-              f -> {
-                String name = f.getName();
-                return name.substring(0, name.length() - 5);
-              })
-          .toList();
+        }
+
+        public List<String> getConfigFileNames() {
+            File[] files = Config.listConfigs();
+            return Arrays.stream(files)
+                         .map(f -> {
+                             String name = f.getName();
+                             return name.substring(0, name.length() - 5);
+                         })
+                         .toList();
+        }
     }
-  }
 
-  Context context = new Context();
+    Context context = new Context();
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+    public static void main (String[] args){
+        launch(args);
+    }
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    MenuBar menuBar = new MenuBar();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        MenuBar menuBar = new MenuBar();
 
-    // Created menus at the top of GUI
-    Menu fileMenu = new Menu("File");
-    Menu configMenu = new Menu("Config");
-    Menu tasksMenu = new Menu("Tasks");
-    Menu helpMenu = new Menu("Help");
+        //Created menus at the top of GUI
+        Menu fileMenu = new Menu("File");
+        Menu configMenu = new Menu("Config");
+        Menu tasksMenu = new Menu("Tasks");
+        Menu helpMenu = new Menu("Help");
 
-    // Menu items for file Menu
-    MenuItem run = new MenuItem("Run...");
-    MenuItem directory = new MenuItem("Set directory");
+        //Menu items for file Menu
+        MenuItem run = new MenuItem("Run...");
+        MenuItem directory = new MenuItem("Set directory");
 
-    // Retrieves all file menu items into the fileMenu
-    fileMenu.getItems().addAll(run, directory);
+        //Retrieves all file menu items into the fileMenu
+        fileMenu.getItems().addAll(
+                run,
+                directory
+        );
 
-    // Menu items for configMenu
-    MenuItem newConfig = new MenuItem("Create new config");
-    Menu loadConfig = new Menu("Load config");
-    MenuItem save = new MenuItem("Save");
-    MenuItem saveAs = new MenuItem("Save As...");
-    MenuItem close = new MenuItem("Close");
-    MenuItem pref = new MenuItem("Preferences...");
-    MenuItem quit = new MenuItem("Quit");
+        //Menu items for configMenu
+        MenuItem newConfig = new MenuItem("Create new config");
+        Menu loadConfig = new Menu("Load config");
+        MenuItem save = new MenuItem("Save");
+        MenuItem saveAs = new MenuItem("Save As...");
+        MenuItem close = new MenuItem("Close");
+        MenuItem pref = new MenuItem("Preferences...");
+        MenuItem quit = new MenuItem("Quit");
 
-    // Retrieves all config menu items into the configMenu
-    configMenu
-        .getItems()
-        .addAll(
-            newConfig,
-            loadConfig,
-            new SeparatorMenuItem(),
-            save,
-            saveAs,
-            close,
-            new SeparatorMenuItem(),
-            pref,
-            quit);
 
-    // Menu items for taskMenu
-    MenuItem newTask = new MenuItem("New");
-    MenuItem delete = new MenuItem("Delete");
+        loadConfig.getItems().addAll(
+                context.getConfigFileNames().stream().map(MenuItem::new).toList()
+        );
+        loadConfig.getItems().add(new MenuItem("Browse..."));
 
-    // Retrieves all task items into taskMenu
-    tasksMenu.getItems().addAll(newTask, delete);
+        //Retrieves all config menu items into the configMenu
+        configMenu.getItems().addAll(
+                newConfig,
+                loadConfig,
+                save,
+                saveAs,
+                new SeparatorMenuItem(),
+                pref,
+                quit
 
-    // Gathers all the menus to the menuBar
-    menuBar.getMenus().addAll(fileMenu, configMenu, tasksMenu, helpMenu);
+        );
 
-    // Creates the original Layout of GUI
-    BorderPane layout = new BorderPane();
+        //Menu items for taskMenu
+        MenuItem newTask = new MenuItem("New");
+        MenuItem delete = new MenuItem("Delete");
 
-    // Makes a split down the middle with left and right in GUI
-    SplitPane splitPane = new SplitPane();
-    AnchorPane left = new AnchorPane();
-    AnchorPane right = new AnchorPane();
-    splitPane.getItems().addAll(left, right);
+        //Retrieves all task items into taskMenu
+        tasksMenu.getItems().addAll(
+                newTask,
+                delete
+        );
 
-    // Formats where the menu bar and split should go inside GUI
-    layout.setCenter(splitPane);
-    layout.setTop(menuBar);
+        // Gathers all the menus to the menuBar
+        menuBar.getMenus().addAll(fileMenu,configMenu,tasksMenu,helpMenu);
 
-    // Builds scene
-    Scene scene = new Scene(layout, 1024, 768);
-    primaryStage.setTitle("Prototype");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+        //Creates the original Layout of GUI
+        BorderPane layout = new BorderPane();
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+        //root acts as the main background for list view and the output
+        AnchorPane root = new AnchorPane();
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    MenuBar menuBar = new MenuBar();
+        //Splits the listView and VBox
+        HBox hBox = new HBox();
 
-    // Created menus at the top of GUI
-    Menu fileMenu = new Menu("File");
-    Menu configMenu = new Menu("Config");
-    Menu tasksMenu = new Menu("Tasks");
-    Menu helpMenu = new Menu("Help");
+        //All items in the listView
+        ListView<String> listView = new ListView<>();
 
-    // Menu items for file Menu
-    MenuItem run = new MenuItem("Run...");
-    MenuItem directory = new MenuItem("Set directory");
+        //Where the output will be (text and project name)
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.TOP_CENTER);
 
-    // Retrieves all file menu items into the fileMenu
-    fileMenu.getItems().addAll(run, directory);
+        //Current project name
+        Label projectName = new Label("Project name");
 
-    // Menu items for configMenu
-    MenuItem newConfig = new MenuItem("Create new config");
-    Menu loadConfig = new Menu("Load config");
-    MenuItem save = new MenuItem("Save");
-    MenuItem saveAs = new MenuItem("Save As...");
-    MenuItem close = new MenuItem("Close");
-    MenuItem pref = new MenuItem("Preferences...");
-    MenuItem quit = new MenuItem("Quit");
+        //Where the text will be displayed
+        AnchorPane miniPane = new AnchorPane();
 
-    loadConfig.getItems().addAll(context.getConfigFileNames().stream().map(MenuItem::new).toList());
-    loadConfig.getItems().add(new MenuItem("Browse..."));
+        //output text
+        Text output = new Text("1. Task name");
+        AnchorPane.setTopAnchor(output, 0.0);
+        AnchorPane.setLeftAnchor(output, 0.0);
 
-    // Retrieves all config menu items into the configMenu
-    configMenu
-        .getItems()
-        .addAll(newConfig, loadConfig, save, saveAs, new SeparatorMenuItem(), pref, quit);
+        //Puts the text into the Anchor pane
+        miniPane.getChildren().add(output);
 
-    // Menu items for taskMenu
-    MenuItem newTask = new MenuItem("New");
-    MenuItem delete = new MenuItem("Delete");
+        //Puts the project name and anchor pane (which holds the text) into VBOX
+        vBox.getChildren().addAll(projectName, miniPane);
 
-    // Retrieves all task items into taskMenu
-    tasksMenu.getItems().addAll(newTask, delete);
+        //Separates the listView and VBOX
+        hBox.getChildren().addAll(listView, vBox);
 
-    // Gathers all the menus to the menuBar
-    menuBar.getMenus().addAll(fileMenu, configMenu, tasksMenu, helpMenu);
+        //Finally puts everything into a single anchorpane
+        root.getChildren().add(hBox);
 
-    // Creates the original Layout of GUI
-    BorderPane layout = new BorderPane();
+        AnchorPane.setTopAnchor(hBox, 0.0);
+        AnchorPane.setBottomAnchor(hBox, 0.0);
+        AnchorPane.setRightAnchor(hBox, 0.0);
+        AnchorPane.setLeftAnchor(hBox, 0.0);
 
-    // root acts as the main background for list view and the output
-    AnchorPane root = new AnchorPane();
 
-    // Splits the listView and VBox
-    HBox hBox = new HBox();
+        //Formats where the menu bar and split should go inside GUI
+        layout.setCenter(root);
+        layout.setTop(menuBar);
 
-    // All items in the listView
-    ListView<String> listView = new ListView<>();
-
-    // Where the output will be (text and project name)
-    VBox vBox = new VBox();
-    vBox.setAlignment(Pos.TOP_CENTER);
-
-    // Current project name
-    Label projectName = new Label("Project name");
-
-    // Where the text will be displayed
-    AnchorPane miniPane = new AnchorPane();
-
-    // output text
-    Text output = new Text("1. Task name");
-    AnchorPane.setTopAnchor(output, 0.0);
-    AnchorPane.setLeftAnchor(output, 0.0);
-
-    // Puts the text into the Anchor pane
-    miniPane.getChildren().add(output);
-
-    // Puts the project name and anchor pane (which holds the text) into VBOX
-    vBox.getChildren().addAll(projectName, miniPane);
-
-    // Separates the listView and VBOX
-    hBox.getChildren().addAll(listView, vBox);
-
-    // Finally puts everything into a single anchorpane
-    root.getChildren().add(hBox);
-
-    AnchorPane.setTopAnchor(hBox, 0.0);
-    AnchorPane.setBottomAnchor(hBox, 0.0);
-    AnchorPane.setRightAnchor(hBox, 0.0);
-    AnchorPane.setLeftAnchor(hBox, 0.0);
-
-    // Formats where the menu bar and split should go inside GUI
-    layout.setCenter(root);
-    layout.setTop(menuBar);
-
-    // Builds scene
-    Scene scene = new Scene(layout, 1024, 768);
-    primaryStage.setTitle("Prototype");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+        //Builds scene
+        Scene scene = new Scene (layout, 1024, 768);
+        primaryStage.setTitle("Prototype");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
