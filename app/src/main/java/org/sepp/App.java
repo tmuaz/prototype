9c1da9d30ec617ebe9d796d759b255d4c6821e25
Muaz <tahasin-muaz@outlook.com>
2024-12-11 18:25:13 +0000

    remove test argument

--- app/src/main/java/org/sepp/App.java
+++ app/src/main/java/org/sepp/App.java
@@ -3,8 +3,6 @@ package org.sepp;
 public class App {
 
   public static void main(String[] args) {
-    // to nahir: do check to run cli/gui
-    args = new String[] {"-i"};
     if (args.length == 0) {
       GUI.main(args);
     } else {
